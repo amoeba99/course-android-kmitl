2d51eb87a9976c2f645b89470d83d47d17098b7a
Unknown <kuro-neko@hotmail.co.th>
2017-10-25 23:05:04 +0700

    Implement clear preference before test

--- lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTest.java
+++ lab/lab08/Espresso/app/src/androidTest/java/com/project/demorecord/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.project.demorecord;
 
+import android.content.Context;
 import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -13,10 +15,13 @@ import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+import java.io.File;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -32,6 +37,15 @@ import static com.project.demorecord.TestUtils.withRecyclerView;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Before
+    public void strat(){
+        File root = InstrumentationRegistry.getTargetContext().getFilesDir().getParentFile();
+        String[] sharedPreferencesFileNames = new File(root, "shared_prefs").list();
+        for (String fileName : sharedPreferencesFileNames) {
+            InstrumentationRegistry.getTargetContext().getSharedPreferences(fileName.replace(".xml", ""), Context.MODE_PRIVATE).edit().clear().commit();
+        }
+    }
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
